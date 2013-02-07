a32714ffa669678471178d9142495c5c9ae52496
roofis0 <roofis20002003@yahoo.com>
2013-02-06 23:25:56 -0500

    finished ex 1, 2 , 3

--- lab1/ex1.c
+++ lab1/ex1.c
@@ -17,6 +17,18 @@ main(int argc, char **argv)
   // the value of x by 1 using in-line assembly
   //
 
+
+
+asm("movl %1, %%eax;"
+     "inc %%eax;"
+     "movl %%eax, %0;"
+     :"=a"(x)
+     :"a"(x)
+     //:"%eax"
+);
+
+
+
   printf("Hello x = %d after increment\n", x);
 
   if(x == 2){

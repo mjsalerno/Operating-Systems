fae87b2a5010e1e4755cb415e86d9411867d64a9
roofis0 <roofis20002003@yahoo.com>
2013-02-23 20:06:07 -0500

    made a function that searched the enviornmental variables

--- lab2/shellhelper.c
+++ lab2/shellhelper.c
@@ -1,5 +1,33 @@
 #include "shellhelper.h"
+#include <string.h>
+#include <stdio.h>
 
 char* parseEnv(char **envp, char *keyword){
-  return NULL;
+  char *cp, *at;
+  int i;
+  
+  for(at = *envp, i = 0; envp+i != NULL; at = (*(envp)+i), i++){
+	  cp = strstr(at, keyword);
+	  
+	  if(cp != NULL)
+		break;  
+	}
+	
+	if(envp+i == '\0')
+		return NULL;
+	else{
+		// USE i AND return char pointer from envp
+		return strstr(at, "=")+1;
+	}
+	
+}
+
+int main (int argc, char ** argv, char **envp) {
+	
+	char *cp = parseEnv(envp, "PATH");
+	
+	printf("\nGOT: %s\n", cp);
+	//printf("\nGOT: %s\n", strstr("HELLO HI", "L"));
+	
+	return 0;
 }

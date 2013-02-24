74954c5edb8c0fbdd2550627a13be94b680b64f8
Paul Campbell <paul.campbell@stonybrook.edu>
2013-02-23 19:51:14 -0500

    Implemented the spawn function. Adjusted the shellhelper.h to include the prototype. Added documentation to the shellhelper.h file.

--- lab2/shellhelper.c
+++ lab2/shellhelper.c
@@ -1,6 +1,21 @@
+#include <unistd.h>
+#include <sys/types.h>
 #include "shellhelper.h"
 
 char* parseEnv(char **envp, char *keyword){
   return NULL;
+}
+
+int spawn(char *program, char **args){
+  pid_t pid;
+  pid = fork();
+  if(pid != 0)
+    return pid;
+  else{
+    execvp(program, args);
+    // If something went wrong while trying to execute to program print out an error and quit.
+    fprintf(stderr, "An error occured while trying to start '%s'\n", program);
+    abort();
+  }
 }
 
--- lab2/shellhelper.h
+++ lab2/shellhelper.h
@@ -2,13 +2,17 @@
 #define SHELLHELPER_H
 /**
  * Searchs the array containing the enviroment variables and looks for the provided keyword.
+ * @param envp NULL terminated array of char* that contains all the enviroment variables.
+ * @param keyword char array containing a sequence of characters to search for.
  * @return Returns a pointer to the char array if it is found, else NULL.
  */
 char* parseEnv(char **envp, char *keyword);
 
 /**
  * Performs fork & exec using the program, and arguments provided.
+ * @param program char array containing the name of the binary to be executed.
+ * @param args NULL terminated array of char* that contains the argument(s) to the binary.
  * @return Returns the pid of the child that is spawned.
  */
-int spawn(char *binary, char **args);
+int spawn(char *program, char **args);
 #endif

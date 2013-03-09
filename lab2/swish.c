3aab4953dfe5bf36adb922dcac5d685dc5148be1
Paul Campbell <paul.campbell@stonybrook.edu>
2013-03-08 22:01:18 -0500

    Adjust the code for printing and reading the prompt in the shell. Added std=c99 to the make file. Replace TRUE and FALSE macros with the c99 stdbool.h header file.

--- lab2/swish.c
+++ lab2/swish.c
@@ -3,54 +3,35 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include "shellhelper.h"
 
 // Assume no input line will be longer than 1024 bytes
 #define MAX_INPUT 1024
 #define MAX_ARGS  1024
 #define MAX_PATH  2048
-#define TRUE  1
-#define FALSE 0
 
 int main (int argc, char ** argv, char **envp) {
 
-  int finished = 0;
+  bool finished = false;
   char *prompt = "\033[0;32mswish>\033[0m ";
   char cmd[MAX_INPUT];
   char *arguments[MAX_ARGS];
   setbuf(stdout, NULL);
 
   while (!finished) {
-    char *cursor;
-    char last_char;
-    int rv;
-    int count;
     char wd[MAX_PATH];
 
+    // Print out the prompt
     getcwd(wd, MAX_PATH);  
-    printf("%s[%s]%s ", BLUE, wd, NONE);
-    rv = write(1, prompt, strlen(prompt));
-    if (!rv) { 
-      finished = 1;
-      break;
-    }
-
+    printf("%s[%s]%s %s ", BLUE, wd, NONE, prompt);
+    gets(cmd);
     
-    // read and parse the input
-    for(rv = 1, count = 0, cursor = cmd, last_char = 1; rv && (++count < (MAX_INPUT-1)) && (last_char != '\n'); cursor++) { 
-      rv = read(0, cursor, 1);
-      last_char = *cursor;
-    } 
-    *(cursor-1) = '\0';
-
-    if (!rv) { 
-      finished = 1;
-      break;
-    }
+    // Evaluate the command
     if(strlen(cmd)){
       parseCommand(cmd, arguments, MAX_ARGS);
       if(!strcmp(arguments[0], "exit")){
-        finished = TRUE;      
+        finished = true;      
       }else if(!strcmp(arguments[0], "cd")){
         int val = chdir(arguments[1]);
         if(val) printf("Sorry but %s does not exist\n", arguments[1]);
@@ -59,9 +40,6 @@ int main (int argc, char ** argv, char **envp) {
         spawn(arguments);
       }
     }       
-    // Execute the command, handling built-in commands separately 
-    // Just echo the command line for now
-    // write(1, cmd, strnlen(cmd, MAX_INPUT));
   }
 
   return 0;

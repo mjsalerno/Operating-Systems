26c28ea78cd4b4e9526907cd57c3b26d5083fab5
michael salerno <roofis20002003@yahoo.com>
2013-03-10 14:39:00 -0400

    fixed indents

--- lab2/swish.c
+++ lab2/swish.c
@@ -12,61 +12,61 @@
 #define MAX_ARGS  1024
 #define MAX_PATH  2048
 
-int main (int argc, char ** argv, char **envp) {
+int main(int argc, char ** argv, char **envp) {
 
-  bool finished = false;
-  char *prompt = "\033[0;32mswish>\033[0m ";
-  int debug = 0;
-  char cmd[MAX_INPUT];
-  char *arguments[MAX_ARGS];
-  setbuf(stdout, NULL);
-  char tmpStr1[MAX_PATH] = {'\0'};
-  char tmpStr2[MAX_PATH] = {'\0'};
+    bool finished = false;
+    char *prompt = "\033[0;32mswish>\033[0m ";
+    int debug = 0;
+    char cmd[MAX_INPUT];
+    char *arguments[MAX_ARGS];
+    setbuf(stdout, NULL);
+    char tmpStr1[MAX_PATH] = {'\0'};
+    char tmpStr2[MAX_PATH] = {'\0'};
 
-  if(argc > 1){
-    if(!strcmp("-d", argv[1])){
-      debug = 1;
-      printf("debugging on.\n");
-    }
-  }
-
-  while (!finished) {
-    char wd[MAX_PATH];
-
-    // Print out the prompt
-    getcwd(wd, MAX_PATH);  
-    printf("%s[%s]%s %s ", BLUE, wd, NONE, prompt);
-    fgets(cmd, MAX_INPUT, stdin);
-    strtok(cmd, "\n"); // Thread safe?
-    
-    // Evaluate the command
-    if(strlen(cmd)){
-      if(debug)printf("RUNNING:%s\n", cmd);
-      parseCommand(cmd, arguments, MAX_ARGS);
-      if(!strcmp(arguments[0], "exit")){
-        finished = true;      
-      }else if(!strcmp(arguments[0], "cd")){
-        strcpy(tmpStr1, "OLDPWD=");
-        getcwd(tmpStr2, MAX_PATH);
-        strcat(tmpStr1, tmpStr2);
-        printf("tmpStr: %s\n", tmpStr1);
-        putenv(tmpStr1);
-        if(!strcmp(arguments[1], "-")){
-          //putenv("OLDPWD=/");
-          chdir(getenv("OLDPWD"));
-        }else if(!strcmp(arguments[1], "~")){
-          chdir(parseEnv(envp, "HOME"));
-        }else{
-        int val = chdir(arguments[1]);
-        if(val) printf("Sorry but %s does not exist\n", arguments[1]);
-        //STILL NEEDS - IMPLEMENTATION
+    if (argc > 1) {
+        if (!strcmp("-d", argv[1])) {
+            debug = 1;
+            printf("debugging on.\n");
         }
-      }else{
-        spawn(arguments);
-      }
-      if(debug)printf("ENDED: %s (needs return val)\n", cmd);
-    }       
-  }
+    }
 
-  return 0;
+    while (!finished) {
+        char wd[MAX_PATH];
+
+        // Print out the prompt
+        getcwd(wd, MAX_PATH);
+        printf("%s[%s]%s %s ", BLUE, wd, NONE, prompt);
+        fgets(cmd, MAX_INPUT, stdin);
+        strtok(cmd, "\n"); // Thread safe?
+
+        // Evaluate the command
+        if (strlen(cmd)) {
+            if (debug)printf("RUNNING:%s\n", cmd);
+            parseCommand(cmd, arguments, MAX_ARGS);
+            if (!strcmp(arguments[0], "exit")) {
+                finished = true;
+            } else if (!strcmp(arguments[0], "cd")) {
+                strcpy(tmpStr1, "OLDPWD=");
+                getcwd(tmpStr2, MAX_PATH);
+                strcat(tmpStr1, tmpStr2);
+                printf("tmpStr: %s\n", tmpStr1);
+                putenv(tmpStr1);
+                if (!strcmp(arguments[1], "-")) {
+                    //putenv("OLDPWD=/");
+                    chdir(getenv("OLDPWD"));
+                } else if (!strcmp(arguments[1], "~")) {
+                    chdir(parseEnv(envp, "HOME"));
+                } else {
+                    int val = chdir(arguments[1]);
+                    if (val) printf("Sorry but %s does not exist\n", arguments[1]);
+                    //STILL NEEDS - IMPLEMENTATION
+                }
+            } else {
+                spawn(arguments);
+            }
+            if (debug)printf("ENDED: %s (needs return val)\n", cmd);
+        }
+    }
+
+    return 0;
 }

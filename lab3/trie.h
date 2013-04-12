0224b06810256432e553f41e5900d6342a0e0994
Mike <roofis20002003@yahoo.com>
2013-04-12 16:40:38 -0400

    changed the indentation of files

--- lab3/trie.h
+++ lab3/trie.h
@@ -7,28 +7,28 @@
 /* A simple (reverse) trie interface */
 
 /* Optional init routine.  May not be required. */
-void init (int numthreads);
+void init(int numthreads);
 
 /* Return 1 on success, 0 on failure */
-int insert (const char *string, size_t strlen, int32_t ip4_address);
+int insert(const char *string, size_t strlen, int32_t ip4_address);
 
 /* Return 1 if the key is found, 0 if not. 
  * If ip4_address is not NULL, store the IP 
  * here.  
  */
-int search  (const char *string, size_t strlen, int32_t *ip4_address);
+int search(const char *string, size_t strlen, int32_t *ip4_address);
 
 /*
  * Same as search except there is no locks within this function.
  * (locking is done from the outside.)
  */
-int squat_search  (const char *string, size_t strlen, int32_t *ip4_address);
+int squat_search(const char *string, size_t strlen, int32_t *ip4_address);
 
 /* Return 1 if the key is found and deleted, 0 if not. */
-int delete  (const char *string, size_t strlen);
+int delete (const char *string, size_t strlen);
 
 /* Print the structure of the tree.  Mostly useful for debugging. */
-void print (); 
+void print();
 
 /* Determines whether to allow blocking until 
  * a name is available.
